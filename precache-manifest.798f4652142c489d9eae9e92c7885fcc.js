09992cb15b2bf45a52d1897ee3ea91d6363f16d1
cirrost292 <cirrushrs@gmail.com>
2020-03-17 21:18:10 +0900

    Updates

---
+++ precache-manifest.798f4652142c489d9eae9e92c7885fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef390cac41b0f877bf5de702637dbd0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fce8c06ca57c674c5e35",
+    "url": "/movie_app/static/css/main.8f8429f9.chunk.css"
+  },
+  {
+    "revision": "28e07b8137429b085d21",
+    "url": "/movie_app/static/js/2.fcc0391f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.fcc0391f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce8c06ca57c674c5e35",
+    "url": "/movie_app/static/js/main.626a2ddf.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
